18c4e126129869b098681c9396895e9274768241
dbybek <bibekswainfriends99@gmail.com>
2026-01-23 20:34:01 +0530

    Changes to withdraw and deposit methods.

--- src/main/java/IntroToOOP/BankAccount.java
+++ src/main/java/IntroToOOP/BankAccount.java
@@ -5,17 +5,23 @@ public class BankAccount {
     String ownerName;
 
     void withdraw(double amount){
-        if(amount>this.balance){
-            System.out.println("Not sufficient balance.");
+        if(amount<=0){
+            System.out.println("Please enter a valid amount greater than 0.");
         }
-        else{
+        if(amount<=this.balance){
             this.balance -= amount;
             System.out.println("Rs."+amount+"/- debited from your account.");
             System.out.println("Remaining balance: Rs."+this.balance+"/-");
         }
+        else {
+            System.out.println("Not sufficient balance.");
+        }
     }
 
     void deposit(double amount){
+        if(amount<=0){
+            System.out.println("Please enter a valid amount greater than 0.");
+        }
         this.balance += amount;
         System.out.println("Rs."+amount+"/- credited to your account.");
         System.out.println("Total balance: Rs."+this.balance+"/-");
